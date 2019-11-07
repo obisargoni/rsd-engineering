d049c74715781830800ff3a96aca0b3985871095
obisargoni <othompsarg@hotmail.com>
2019-11-07 11:54:54 +0000

    Renaming of variable s to branch length. Clarifying creation of first coordinates

--- fractal_tree.py
+++ fractal_tree.py
@@ -1,15 +1,24 @@
 from math import sin, cos
 from matplotlib import pyplot as plt
-s=1
+
+branch_length=1
+
+alpha = 0
+
 d=[[0,1,0]]
-plt.plot([0,0],[0,1])
+
+c0 = [0,0]
+c1 = [branch_length*sin(alpha), branch_length*cos(alpha)]
+
+plt.plot(c0,c1)
 for i in range(5):
     n=[]
     for j in range(len(d)):
-        n.append([d[j][0]+s*sin(d[j][2]-0.2), d[j][1]+s*cos(d[j][2]-0.2), d[j][2]-0.2])
-        n.append([d[j][0]+s*sin(d[j][2]+0.2), d[j][1]+s*cos(d[j][2]+0.2), d[j][2]+0.2])
+        n.append([d[j][0]+branch_length*sin(d[j][2]-0.2), d[j][1]+branch_length*cos(d[j][2]-0.2), d[j][2]-0.2])
+        n.append([d[j][0]+branch_length*sin(d[j][2]+0.2), d[j][1]+branch_length*cos(d[j][2]+0.2), d[j][2]+0.2])
         plt.plot([d[j][0], n[-2][0]],[d[j][1], n[-2][1]])
         plt.plot([d[j][0], n[-1][0]],[d[j][1], n[-1][1]])
     d=n
-    s*=0.6
+    branch_length*=0.6
 plt.savefig('tree.png')
+
