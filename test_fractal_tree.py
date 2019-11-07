360b83b065d3874316bcd614bd1a17152c70a670
obisargoni <othompsarg@hotmail.com>
2019-11-07 11:54:08 +0000

    Regression test for fractal script

---
+++ test_fractal_tree.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 11:47:54 2019
+
+@author: student
+"""
+
+# Write regression test for fractal tree
+
+# Import data that is produced by fractal tree
+from fractal_tree import d
+import pickle
+
+def test_fractal_tree():
+    orig_d_path = "nFinal.data"
+    with open(orig_d_path, "rb") as f:
+        orig_d = pickle.load(f)
+        assert orig_d == d
