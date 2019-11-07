bfef80d6f40550696d9fbca9fd9673c17000f3f5
obisargoni <othompsarg@hotmail.com>
2019-11-07 13:53:01 +0000

    Refactor of the coordiante frmo previous iteration

--- fractal_tree.py
+++ fractal_tree.py
@@ -14,8 +14,18 @@ plt.plot(c0,c1)
 for i in range(5):
     n=[]
     for j in range(len(d)):
-        n.append([d[j][0]+branch_length*sin(d[j][2]-0.2), d[j][1]+branch_length*cos(d[j][2]-0.2), d[j][2]-0.2])
-        n.append([d[j][0]+branch_length*sin(d[j][2]+0.2), d[j][1]+branch_length*cos(d[j][2]+0.2), d[j][2]+0.2])
+        aMinus = d[j][2] - 0.2
+        aPlus = d[j][2] + 0.2
+        
+        cDJ = d[j][0:2]
+        
+        # get two new coordinates based on the previous coordinates
+        cMinus = [cDJ[0]+branch_length*sin(aMinus), cDJ[1]+branch_length*cos(aMinus)]
+        n.append(cMinus + [aMinus])
+        
+        cPlus = [cDJ[0]+branch_length*sin(aPlus), cDJ[1]+branch_length*cos(aPlus)]
+        n.append(cPlus + [aPlus])
+        
         plt.plot([d[j][0], n[-2][0]],[d[j][1], n[-2][1]])
         plt.plot([d[j][0], n[-1][0]],[d[j][1], n[-1][1]])
     d=n
